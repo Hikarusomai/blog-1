b4767f553e16b94a30b9e838e392356bf5f5d0e6
Hikarusomai <bouchoufisalim@gmail.com>
2023-11-07 19:21:10 +0100

    add js script google manager

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -124,6 +124,15 @@
 
     <!-- Java Script
     ================================================== -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZBNZ0CEGFC"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-ZBNZ0CEGFC');
+    </script>
     <script src="{{ asset('front/js/jquery-3.5.0.min.js') }}"></script>
     <script src="{{ asset('front/js/plugins.js') }}"></script>
     <script src="{{ asset('front/js/main.js') }}"></script>
