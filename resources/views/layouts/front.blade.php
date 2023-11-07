bd297cb4674e94f5ba7dd606113aba279794aa77
Hikarusomai <bouchoufisalim@gmail.com>
2023-11-07 19:09:52 +0100

    add google site verif

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -31,7 +31,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
     <link rel="manifest" href="{{ asset('front/site.webmanifest') }}">
-
+    <meta name="google-site-verification" content="J-AXxosAyFbO9vIP0zo1eVjjxOukestnkV0iCnyKuB0" />
 </head>
 
 <body id="top">
